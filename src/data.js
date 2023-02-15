d0c00ba171c0c823f53ad927dd61c2fc0c7dd5b4
varaprasadgarbham <125496829+varaprasadgarbham@users.noreply.github.com>
2023-02-16 00:56:11 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'MacBook',
+      price: 140,
+      image: 'https://picsum.photos/id/180/2400/1600',
+    },
+    {
+      id: '2',
+      name: 'Old Car',
+      price: 240,
+      image: 'https://picsum.photos/id/111/4400/2656',
+    },
+    {
+      id: '3',
+      name: 'W Shoes',
+      price: 100,
+      image: 'https://picsum.photos/id/21/3008/2008',
+    },
+  ],
+};
+export default data;
